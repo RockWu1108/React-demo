3edde4f569b7bc20e21bdec467f5f6b805e589ac
RockWu <any70294@yahoo.com.tw>
2021-04-27 12:15:18 +0800

    Drawer Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
+import Grid from './pages/grid';
+import MarerialUIDrawer from './components/drawer';
+import { BrowserRouter, Route, Switch }	from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+return (
+	<div className="App">	
+  {/* Drawer*/}
+  <MarerialUIDrawer/>
 
-export default App;
+    <Switch>
+        <Route path="/" exact >
+            <Home/>
+        </Route>
+        <Route exact path='/grid'>
+            <Grid/>
+        </Route>
+    </Switch>
+	</div>
+);
+}
+export default App ;
